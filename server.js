c40dd8205f40962c292177c944f316851365e931
jayms8462 <jayms.portune@gmail.com>
2019-08-15 21:27:18 -0700

    Pushing Data for the Day

--- server.js
+++ server.js
@@ -12,41 +12,36 @@ app.use(cors());
 app.use(express.static("./public/../"));
 app.use(express.urlencoded({ extended: true }));
 
-function Book(book) {
-  // Prevent mixed-content warnings
-  this.image = "https" + book.volumeInfo.imageLinks.thumbnail.slice(4);
-  this.title = book.volumeInfo.title;
-  this.author = book.volumeInfo.authors;
-  this.description = book.volumeInfo.description;
-}
-
 app.get("/", (request, response) => {
-  response.render("pages/index");
+  try {
+    response.render("pages/index");
+  } catch {
+    handleError(err)
+  }
 });
 
 app.get("/hello", (request, response) => {
-  response.render("pages/index");
+  try {
+    response.render("pages/index");
+  } catch {
+    handleError(err)
+  }
 });
+
+app.get('*', handleError);
+
 
 app.post("/search", (request, response) => {
-  let url = "https://www.googleapis.com/books/v1/volumes?q=";
+  let url = "http://www.googleapis.com/books/v1/volumes?q=";
   request.body.search[1] === "author"
-    ? (url += `inauthor:${request.body.search[0]}&maxResults=20`)
-    : (url += `intitle:${request.body.search[0]}&maxResults=20`);
-  console.log("URL!!!! ", url);
-
-  return superagent
-    .get(url)
-    .then(bookList => {
-      return bookList.body.items.map(book => new Book(book));
-    })
-    .then(results => {
-      console.log("RESOOOOO", results);
-      response.render("pages/searches/show", { bookResults: results });
-    })
-    .catch(error => {
-      console.log("Superagent Error: ", error);
-    });
+    ? (url += `inauthor:${request.body.search[0]}`)
+    : (url += `intitle:${request.body.search[0]}`);
 });
+
+function handleError(request, response, err){
+  debugger;
+  response.redirect('pages/error');
+}
+
 
 app.listen(PORT, () => console.log(`Listening on port number ${PORT}`));
